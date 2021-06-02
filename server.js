688102e30fa37ddc23b654dca800c97eb5b946d9
DipCoy <dipolarcoyote@gmail.com>
2021-06-02 20:54:48 +0500

    Server works

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-
-let server = http.createServer((req, res) => {
-    console.log(req.url);
-    console.log(req.method);
-});
+const server = express();
 
 // TODO: Прописать роутинг.
 // TODO: Примеры который уже загружены поместить в /examples и брать по имени от туда
@@ -13,22 +8,6 @@ let server = http.createServer((req, res) => {
 // TODO: Пока без авторизации\пользователей и тд
 
 // TODO: После выбора или загрузки картинки уметь переходить на страничку с редактингом
-server.on('request', (req, res) => {
-    switch (req.url) {
-        case '/':
-            res.end(fs.readFileSync('index.html'));
-            //При каждом запросе файл будет считываться заново, а значит можно редачить не перезапуская сервер
-            return;
-
-        case '/photo':
-            res.end(fs.readFileSync(`./photo/824439.700xp.jpg`));
-            return;
-
-        default:
-            res.writeHead(404);
-            res.end();
-            return;
-    }
-});
-
+server.use(express.static('static'));
+server.use('/photo', express.static('photo'));
 server.listen(800, 'localhost');
---
+++ static/editor.js
@@ -0,0 +1,32 @@
+
+// теперь мы можем импортировать файл и editor.js вернет нам функцию editor
+
+let canv=document.getElementById("img"),
+    c=canv.getContext("2d");
+function onfill(doc)
+{
+    var file=doc.files[0],
+        fileread=new FileReader();
+    fileread.onload=function()
+    {
+        var img=new Image();
+        img.src=fileread.result;
+        img.onload=function()
+        {
+            canv.width=img.width;
+            canv.height=img.height;
+            c.drawImage(img,0,0);
+            c.font="60px Arial";
+            var text="тут текст";
+            c.fillText("тут текст",canv.width/2-text.length/2*30,canv.height/2-15);
+        }
+    }
+    fileread.readAsDataURL(file);
+}
+function get()
+{
+    var link=document.createElement("a");
+    link.download="download";
+    link.href=canv.toDataURL(["image/png"]);
+    link.click();
+}
